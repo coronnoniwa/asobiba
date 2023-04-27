e4de721e41ed2d6a37303f1d8dd26651cd3c8563
coronnoniwa <musasi110604@gmail.com>
2023-04-27 23:18:42 +0900

    施設一覧ページ作成

---
+++ db/migrate/20230424114816_create_facilities.rb
@@ -0,0 +1,13 @@
+class CreateFacilities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :facilities do |t|
+      t.string :name
+      t.integer :prefecture_id
+      t.integer :city_id
+      t.string :facility_link
+      t.string :place_link
+
+      t.timestamps
+    end
+  end
+end
